7194bad8b252a2441c13dcc57e9756266b3b5577
Sergei <deazmont@yandex.ru>
2022-04-26 20:21:27 +0300

    Программа переделана под условия задачи

--- SeleniumStepik/test_abs_project.py
+++ SeleniumStepik/test_abs_project.py
@@ -1,12 +1,44 @@
 import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+def registration_form(link="http://suninjuly.github.io/registration1.html"):
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    first_name = browser.find_element(By.XPATH, "//input[@placeholder = 'Input your first name']")
+    first_name.send_keys("Ivan")
+
+    second_name = browser.find_element(By.XPATH, "//input[@placeholder = 'Input your last name']")
+    second_name.send_keys("Petrov")
+
+    email = browser.find_element(By.XPATH, "//input[@placeholder = 'Input your email']")
+    email.send_keys("sample@mail.ru")
+
+    browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+
+    time.sleep(1)
+
+    welcome_text = browser.find_element(By.TAG_NAME, "h1").text
+
+    time.sleep(5)
+    browser.quit()
+
+    return welcome_text
 
 
 class TestAbs(unittest.TestCase):
     def test_abs1(self):
-        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+        welcome_text = registration_form("http://suninjuly.github.io/registration1.html")
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!",
+                         "Should be absolute value of a number")
 
     def test_abs2(self):
-        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+        welcome_text = registration_form("http://suninjuly.github.io/registration2.html")
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!",
+                         "Should be absolute value of a number")
 
 
 if __name__ == "__main__":
